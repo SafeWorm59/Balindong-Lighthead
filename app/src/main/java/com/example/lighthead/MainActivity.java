8f19738a4210643f7181095458cb752971cfbc65
SoulAnimation <30415362+SoulAnimation@users.noreply.github.com>
2025-09-01 17:57:58 +0800

    Second Commit

--- app/src/main/java/com/example/lighthead/MainActivity.java
+++ app/src/main/java/com/example/lighthead/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lighthead;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -10,35 +11,81 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button changeBtn;
+    Button startBtn;
+    Handler handler = new Handler();
+    boolean autoMode = false; // to check if auto is running
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RelativeLayout bgElement = findViewById(R.id.activity_main);
-        bgElement.setBackgroundColor(Color.WHITE);
+        RelativeLayout bg = findViewById(R.id.activity_main);
+        bg.setBackgroundColor(Color.WHITE); // default color red
 
-        myButtonListenerMethod();
-    }
+        changeBtn = findViewById(R.id.button);
+        startBtn = findViewById(R.id.startbutton);
 
-    public void myButtonListenerMethod() {
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        changeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RelativeLayout bgElement = findViewById(R.id.activity_main);
-                int color = ((ColorDrawable) bgElement.getBackground()).getColor();
+                RelativeLayout bg = findViewById(R.id.activity_main);
+                int colorNow = ((ColorDrawable) bg.getBackground()).getColor();
 
-                if (color == Color.RED) {
-                    bgElement.setBackgroundColor(Color.BLUE);
-                    button.setText("BLUE");
+                if (colorNow == Color.RED) {
+                    bg.setBackgroundColor(Color.YELLOW);
+                    changeBtn.setText("YELLOW");
+                } else if (colorNow == Color.YELLOW) {
+                    bg.setBackgroundColor(Color.GREEN);
+                    changeBtn.setText("GREEN");
                 } else {
-                    bgElement.setBackgroundColor(Color.RED);
-                    button.setText("RED");
+                    bg.setBackgroundColor(Color.RED);
+                    changeBtn.setText("RED");
                 }
             }
         });
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (autoMode == false) {
+                    autoMode = true;
+                    startBtn.setText("STOP");
+                    changeAutomatically();
+                } else {
+                    autoMode = false;
+                    startBtn.setText("START");
+                    handler.removeCallbacksAndMessages(null);
+                }
+            }
+        });
+    }
+
+    private void changeAutomatically() {
+        if (autoMode == false) {
+            return;
+        }
+
+        RelativeLayout bg = findViewById(R.id.activity_main);
+        int colorNow = ((ColorDrawable) bg.getBackground()).getColor();
+
+        if (colorNow == Color.RED) {
+            bg.setBackgroundColor(Color.YELLOW);
+            changeBtn.setText("YELLOW");
+        } else if (colorNow == Color.YELLOW) {
+            bg.setBackgroundColor(Color.GREEN);
+            changeBtn.setText("GREEN");
+        } else {
+            bg.setBackgroundColor(Color.RED);
+            changeBtn.setText("RED");
+        }
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                changeAutomatically();
+            }
+        }, 1000);
     }
 }
